78bd5edfe76bfcfe795437fe097b976024352bf9
Conqueror Apollos <101530791+ConquerorApollos@users.noreply.github.com>
2022-04-25 15:50:30 -0700

    my commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,14 +14,11 @@ char *_strncpy(char *dest, char *src, int n)
 
 	/**
 	 * iterate through src array
-
 	 * where if there is no null byte
 	 * among the first n bytes of source
 	 * the string placed in dest will not be
 	 * null terminated
 	 */
-
-
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	/**
@@ -34,4 +31,6 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = '\0';
 		i++;
 	}
+
 	return (dest);
+}
